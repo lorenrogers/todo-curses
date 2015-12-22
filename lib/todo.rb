a6cc819de5f403d2165baeb27ff32d93aa7ad75a
Loren Rogers <loren@lorentrogers.com>
2015-12-22 02:26:19 -0500

    Refactored into class file

--- lib/todo.rb
+++ lib/todo.rb
@@ -1,4 +1,8 @@
-require 'todo/version.rb'
+require 'gli'
+require 'todo-txt'
+require 'ncursesw'
 
+require 'todo/version.rb'
+require 'todo/todo_viewer.rb'
 # Add requires for other files you add to your project here, so
 # you just need to require this one file in your bin file
